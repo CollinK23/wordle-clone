b4b5afd7b54e628ba7121cae7c8f4204b25d3978
CollinK23 <kimballcollin@gmail.com>
2022-07-05 02:17:24 -0700

    commit

--- Assets/UI.py
+++ Assets/UI.py
@@ -2,8 +2,11 @@ import pygame
 from .constants import DARK_GREY, WHITE, WIDTH, BLACK, LIGHT_GREY, GREEN, ALPHABET, RED, YELLOW
 pygame.init()
 
-FONT = pygame.font.Font('C:/Users/kimba/wordlegame/wordle/HelveticaNeue-Medium-11.ttf', 45)
-SUBFONT = pygame.font.Font('C:/Users/kimba/wordlegame/wordle/HelveticaNeue-Medium-11.ttf', 24)
+FONT = pygame.font.Font(
+    'C:/Users/kimba/wordguess/Assets/HelveticaNeue-Medium-11.ttf', 45)
+SUBFONT = pygame.font.Font(
+    'C:/Users/kimba/wordguess/Assets/HelveticaNeue-Medium-11.ttf', 24)
+
 
 class Board:
     def draw_entry_box(WIN, color, entry_field, x, y):
@@ -18,19 +21,25 @@ class Board:
                 color = LIGHT_GREY
                 Board.draw_entry_box(WIN, color, entry_field, x, y)
         for col in range(10):
-            pygame.draw.rect(WIN, LIGHT_GREY, [col * 40 + 20, 525, 35, 45], 0, 4)
+            pygame.draw.rect(WIN, LIGHT_GREY, [
+                             col * 40 + 20, 525, 35, 45], 0, 4)
             alphabet_text = SUBFONT.render(ALPHABET[0][col], True, WHITE)
-            text_rect = alphabet_text.get_rect(center=((30 + 40 * col) + 6, 545))
+            text_rect = alphabet_text.get_rect(
+                center=((30 + 40 * col) + 6, 545))
             WIN.blit(alphabet_text, text_rect)
         for col in range(9):
-            pygame.draw.rect(WIN, LIGHT_GREY, [col * 40 + 40, 585, 35, 45], 0, 4)
+            pygame.draw.rect(WIN, LIGHT_GREY, [
+                             col * 40 + 40, 585, 35, 45], 0, 4)
             alphabet_text = SUBFONT.render(ALPHABET[1][col], True, WHITE)
-            text_rect = alphabet_text.get_rect(center=((30 + 40 * col) + 26, 605))
+            text_rect = alphabet_text.get_rect(
+                center=((30 + 40 * col) + 26, 605))
             WIN.blit(alphabet_text, text_rect)
         for col in range(7):
-            pygame.draw.rect(WIN, LIGHT_GREY, [col * 40 + 80, 645, 35, 45], 0, 4)
+            pygame.draw.rect(WIN, LIGHT_GREY, [
+                             col * 40 + 80, 645, 35, 45], 0, 4)
             alphabet_text = SUBFONT.render(ALPHABET[2][col], True, WHITE)
-            text_rect = alphabet_text.get_rect(center=((30 + 40 * col) + 66, 665))
+            text_rect = alphabet_text.get_rect(
+                center=((30 + 40 * col) + 66, 665))
             WIN.blit(alphabet_text, text_rect)
 
     def draw_play_again(WIN):
@@ -41,7 +50,8 @@ class Board:
         WIN.blit(quit_game, (70, 595))
 
     def draw_game_win(WIN):
-        pygame.draw.rect(WIN, BLACK, [WIDTH // 2 - 75, 260 - 25, 150, 50], 0, 4)
+        pygame.draw.rect(
+            WIN, BLACK, [WIDTH // 2 - 75, 260 - 25, 150, 50], 0, 4)
         win_text = SUBFONT.render("Good Job", True, GREEN)
         WIN.blit(win_text, (WIDTH // 2 - 55, 260 - 15))
         Board.draw_play_again(WIN)
@@ -53,14 +63,17 @@ class Board:
         Board.draw_play_again(WIN)
 
     def draw_invalid_input(WIN):
-        pygame.draw.rect(WIN, WHITE, [WIDTH // 2 - 150, 260 - 25, 300, 50], 0, 4)
+        pygame.draw.rect(
+            WIN, WHITE, [WIDTH // 2 - 150, 260 - 25, 300, 50], 0, 4)
         not_word = SUBFONT.render("Not a Word", True, BLACK)
         WIN.blit(not_word, (WIDTH // 2 - 55, 260 - 15))
 
     def draw_indicator(WIN, color, z, row, shift):
-        pygame.draw.rect(WIN, color, [z * 40 + shift, (525 + 60 * row), 35, 45], 0, 4)
+        pygame.draw.rect(
+            WIN, color, [z * 40 + shift, (525 + 60 * row), 35, 45], 0, 4)
         alphabet_text = SUBFONT.render(ALPHABET[row][z], True, WHITE)
-        text_rect = alphabet_text.get_rect(center=((16 + 40 * z) + shift, 545 + 60 * row))
+        text_rect = alphabet_text.get_rect(
+            center=((16 + 40 * z) + shift, 545 + 60 * row))
         WIN.blit(alphabet_text, text_rect)
 
     def indicator_color(WIN, entry_field, turn, real_word_index, x, y, z, row, shift):
--- gamemain.py
+++ gamemain.py
@@ -1,16 +1,18 @@
 import pygame
 import random
-from wordle.constants import WIDTH, HEIGHT, GREEN, YELLOW, WHITE, BLACK
-from wordle.UI import Board, FONT
+from Assets.constants import WIDTH, HEIGHT, GREEN, YELLOW, WHITE, BLACK
+from Assets.UI import Board, FONT
 pygame.init()
 
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Wordle Unlimited")
 
+
 def word_exists(words_list, guess, entry_field, turn):
     if turn > 0:
         turn -= 1
-        guess[turn] = entry_field[turn][0] + entry_field[turn][1] + entry_field[turn][2] + entry_field[turn][3] + entry_field[turn][4]
+        guess[turn] = entry_field[turn][0] + entry_field[turn][1] + \
+            entry_field[turn][2] + entry_field[turn][3] + entry_field[turn][4]
         if any(guess[turn] in word for word in words_list):
             return True
         else:
@@ -18,20 +20,25 @@ def word_exists(words_list, guess, entry_field, turn):
             entry_field[turn] = [" ", " ", " ", " ", " "]
             return False
 
+
 def letters_used(entry_field, turn, word_in_dict, real_word):
     real_word_index = list(real_word)
     if word_in_dict == True:
-            for y in range(0, 5):
-                for x in range(0, 6):
-                    for z in range(10):
-                        row, shift = 0, 20
-                        Board.indicator_color(WIN, entry_field, turn, real_word_index, x, y, z, row, shift)
-                    for z in range(9):
-                        row, shift = 1, 40
-                        Board.indicator_color(WIN, entry_field, turn, real_word_index, x, y, z, row, shift)
-                    for z in range(7):
-                        row, shift = 2, 80
-                        Board.indicator_color(WIN, entry_field, turn, real_word_index, x, y, z, row, shift)
+        for y in range(0, 5):
+            for x in range(0, 6):
+                for z in range(10):
+                    row, shift = 0, 20
+                    Board.indicator_color(
+                        WIN, entry_field, turn, real_word_index, x, y, z, row, shift)
+                for z in range(9):
+                    row, shift = 1, 40
+                    Board.indicator_color(
+                        WIN, entry_field, turn, real_word_index, x, y, z, row, shift)
+                for z in range(7):
+                    row, shift = 2, 80
+                    Board.indicator_color(
+                        WIN, entry_field, turn, real_word_index, x, y, z, row, shift)
+
 
 def letter_check(real_word, entry_field, turn, guess, word_in_dict):
     game_won = False
@@ -39,7 +46,8 @@ def letter_check(real_word, entry_field, turn, guess, word_in_dict):
     if word_in_dict == True:
         for y in range(0, 5):
             for x in range(0, 6):
-                guess[x] = entry_field[x][0] + entry_field[x][1] + entry_field[x][2] + entry_field[x][3] + entry_field[x][4]
+                guess[x] = entry_field[x][0] + entry_field[x][1] + \
+                    entry_field[x][2] + entry_field[x][3] + entry_field[x][4]
                 if (guess[x] in real_word) and (turn > x):
                     color = GREEN
                     Board.draw_entry_box(WIN, color, entry_field, x, y)
@@ -56,22 +64,23 @@ def letter_check(real_word, entry_field, turn, guess, word_in_dict):
                     Board.draw_entry_box(WIN, color, entry_field, x, y)
         return game_won
 
+
 def main():
-    words = open("C:/Users/kimba/wordlegame/five_letters.csv")
+    words = open("C:/Users/kimba/wordguess/five_letters.csv")
     words_list = []
     for row in words:
-        transformed_row = row.replace(",","")
+        transformed_row = row.replace(",", "")
         words_list.append(transformed_row)
-    real_word = words_list[random.randint(1,len(words_list))]
+    real_word = words_list[random.randint(1, len(words_list))]
     words.close()
     print(real_word)
 
     entry_field = [[" ", " ", " ", " ", " "],
-                    [" ", " ", " ", " ", " "],
-                    [" ", " ", " ", " ", " "],
-                    [" ", " ", " ", " ", " "],
-                    [" ", " ", " ", " ", " "],
-                    [" ", " ", " ", " ", " "]]
+                   [" ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " "],
+                   [" ", " ", " ", " ", " "]]
 
     FPS = 60
     run = True
@@ -80,12 +89,13 @@ def main():
     continue_word = True
     game_over = False
     word_in_dict = True
-    guess = [[],[],[],[],[],[]]
+    guess = [[], [], [], [], [], []]
     while run:
         clock.tick(FPS)
         Board.draw_grid(WIN, entry_field)
         letters_used(entry_field, turn, word_in_dict, real_word)
-        game_over = letter_check(real_word, entry_field, turn, guess, word_in_dict)
+        game_over = letter_check(
+            real_word, entry_field, turn, guess, word_in_dict)
         word_in_dict = word_exists(words_list, guess, entry_field, turn)
 
         if turn == 6 and word_in_dict == True and not(guess[turn - 1] in real_word):
@@ -122,4 +132,6 @@ def main():
         pygame.display.update()
     pygame.quit()
     exit()
+
+
 main()
